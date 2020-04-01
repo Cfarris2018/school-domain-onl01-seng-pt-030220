82fa43bb61a5e2518f986d7277d66d110d434a59
Camisha  Farris <cfarris@anointedflooring.com>
2020-04-01 17:12:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,6 @@
 # code here!
 #create a class
-#
-school.roster
+# school should have a roster {} empty hash upon intalization 
+#add student with add_student method with an argument (name,grade)
 
 school.add_student("Zach Morris",9)
